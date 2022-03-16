7eb5c4128f3d0eb1e773ca69a18610d9c11fbff5
Konate Aboubakar <zeibadjo@gmail.com>
2022-03-16 15:55:31 +0000

    Modifier utilisateur

--- src/main/java/ci/mfpma/dq/controller/AdminController.java
+++ src/main/java/ci/mfpma/dq/controller/AdminController.java
@@ -10,13 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ci.mfpma.dq.entites.Utilisateur;
 import ci.mfpma.dq.service.DirectionService;
+import ci.mfpma.dq.service.ProfessionService;
 import ci.mfpma.dq.service.RoleService;
 import ci.mfpma.dq.service.UtilisateurService;
+import ci.mfpma.dq.service.VilleService;
 import ci.mfpma.dq.utilitaires.SendEmailUtil;
 
 @Controller
@@ -28,6 +31,12 @@ public class AdminController {
 	
 	@Autowired
 	DirectionService directionService;
+	
+	@Autowired
+	private ProfessionService professionService;
+	
+	@Autowired
+	private VilleService villeService;
 	
 	@Autowired
 	RoleService roleService;
@@ -53,6 +62,17 @@ public class AdminController {
 		model.addAttribute("roles", roleService.findAllByOrderByNomRoleAsc());
 		return "admin/nouvelUtilisateur";
 	}
+	
+	
+	@GetMapping("/modifierUtilisateur/{id}") 
+	public String getModifierUtilisateur(Model model, @PathVariable(name = "id") Long id) {
+		model.addAttribute("professions", professionService.findAllByOrderByLibelleProfessionAsc());
+		model.addAttribute("directions", directionService.getAllDirections());
+		model.addAttribute("villes", villeService.getAllVilles());
+		model.addAttribute("utilisateur", utilisateurService.getById(id));
+		return "admin/modifierUtilisateur"; 
+	}
+	
 	
 	@PostMapping("/creerUtilisateur")
 	public String creerUtilisateurProcess(Utilisateur utilisateur, Model model) {
